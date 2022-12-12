ed1d1f7291b3b044af4cb83061e97341741e58cd
Jesse Palmer <jesselpalmer@gmail.com>
2022-12-11 19:20:11 -0800

    chore: add build files

---
+++ interpreter/target/debug/deps/interpreter-c0fc2774a5aa6c74.d
@@ -0,0 +1,5 @@
+/Users/jessepalmer/Software/meda/interpreter/target/debug/deps/interpreter-c0fc2774a5aa6c74: src/main.rs
+
+/Users/jessepalmer/Software/meda/interpreter/target/debug/deps/interpreter-c0fc2774a5aa6c74.d: src/main.rs
+
+src/main.rs:
---
+++ interpreter/target/debug/interpreter.d
@@ -0,0 +1,1 @@
+/Users/jessepalmer/Software/meda/interpreter/target/debug/interpreter: /Users/jessepalmer/Software/meda/interpreter/src/main.rs
