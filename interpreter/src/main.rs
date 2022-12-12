60b7d6a77b3111a2560b11d68a284c19866ad6cc
Jesse Palmer <jesselpalmer@gmail.com>
2022-12-11 19:44:43 -0800

    chore: updated args parsing

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+  let args: Vec<String> = env::args().collect();
+
+  if args.len() > 2 {
+    println!("Usage: rmeda [script]");
+  } else if args.len() > 1 {
+    println!("Executing file...");
+  } else {
+    println!("> ");
+  }
 }
---
+++ interpreter/target/debug/deps/interpreter-0b152db0e964d550.d
@@ -0,0 +1,5 @@
+/Users/jessepalmer/Software/meda/interpreter/target/debug/deps/interpreter-0b152db0e964d550.rmeta: src/main.rs
+
+/Users/jessepalmer/Software/meda/interpreter/target/debug/deps/interpreter-0b152db0e964d550.d: src/main.rs
+
+src/main.rs:
