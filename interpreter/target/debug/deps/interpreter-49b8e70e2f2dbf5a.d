9a8628ffe410297115742fce2d5f95263468d1d6
Jesse Palmer <jesselpalmer@gmail.com>
2022-12-18 13:09:48 -0800

    refactor: remove files in gitignore

--- interpreter/target/debug/deps/interpreter-49b8e70e2f2dbf5a.d
+++
@@ -1,5 +0,0 @@
-/Users/jessepalmer/Software/meda/interpreter/target/debug/deps/interpreter-49b8e70e2f2dbf5a.rmeta: src/main.rs
-
-/Users/jessepalmer/Software/meda/interpreter/target/debug/deps/interpreter-49b8e70e2f2dbf5a.d: src/main.rs
-
-src/main.rs:
