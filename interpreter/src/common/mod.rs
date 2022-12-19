e01859fc78eab2fadd6d7dc6ca55cfbc0bd9c9ef
Jesse Palmer <jesselpalmer@gmail.com>
2022-12-18 18:36:28 -0800

    refactor: cleanup files

---
+++ interpreter/src/common/mod.rs
@@ -0,0 +1,10 @@
+pub mod strings {
+  pub mod error_messages {
+    pub const UNABLE_TO_OPEN_FILE: &str = "Should have been able to open file";
+  }
+
+  pub mod cli {
+    pub const PROMPT: &str = "> ";
+    pub const USAGE_TIP: &str = "Usage: rmeda [script]";
+  }
+}
--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,23 +1,41 @@
 use std::env;
 use std::fs;
 
+mod common;
+use common::strings::error_messages::UNABLE_TO_OPEN_FILE;
+use common::strings::cli::{PROMPT, USAGE_TIP};
+
+/// Run file
+fn run_file(file_contents: String) {
+  println!("{}", file_contents);
+}
+
 /// Reads Meda source file
 fn read_file(file_path: String) {
   let contents = fs::read_to_string(file_path)
-       .expect("Should have been able to open file");
+       .expect(UNABLE_TO_OPEN_FILE);
+  
+  run_file(contents);
+}
 
-  println!("Reading file:\n{contents}");
+/// Run REPL
+fn run_repl() {
+  println!("{}", PROMPT);
 }
 
 fn main() {
   let args: Vec<String> = env::args().collect();
+  let too_many_args = args.len() > 2; 
+  let correct_num_args = args.len() == 2;
 
-  if args.len() > 2 {
-    println!("Usage: rmeda [script]");
-  } else if args.len() == 2 {
-    let file_path = args[1].to_string();
+  if too_many_args {
+    println!("{}", USAGE_TIP);
+  } else if correct_num_args {
+    let filepath_arg_index = 1;
+    let file_path = args[filepath_arg_index].to_string();
+
     read_file(file_path);
   } else {
-    println!("> ");
+    run_repl();
   }
 }
