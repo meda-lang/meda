0a3f15270dcf375a82c1c066ad3c547219565501
Jesse Palmer <jesselpalmer@gmail.com>
2022-12-23 14:03:01 -0800

    refactor: rename file

--- interpreter/tests/meda.test.js
+++ interpreter/tests/meda.test.js
@@ -1,7 +1,7 @@
 import {strictEqual} from 'assert';
 import Meda from '../src/meda.js';
 
-describe(`Basic class test suite`, () => {
+describe(`Meda class test suite`, () => {
   it(`should create an instance`, () => {
     strictEqual(typeof new Meda, 'object');
   });
