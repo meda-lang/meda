7ca8b2a006496d1ac08aa8906945c3a0ffbc7f20
Jesse Palmer <jesselpalmer@gmail.com>
2023-01-28 15:29:30 -0800

    task: add args check

--- interpreter_new/src/main.rs
+++ interpreter_new/src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: meda <file_path>");
+        return;
+    }
+
     let file_path = &args[1];
+
     print!("Reading file: {}", file_path);
 }
